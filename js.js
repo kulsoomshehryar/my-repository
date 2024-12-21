3918243ccb9d4688c12e00872a7a25dc5222b5dc
kulsoomshehryar <kulsoomkulsoomshehryar@gmail.com>
2024-12-21 11:23:44 +0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,4 @@
+function mango(k1, k2){
+    return k1*k2;
+}
+  
---
+++ kulsoom.js
@@ -0,0 +1,20 @@
+// Conditional Statements
+let mode = dark;
+if ( mode == dark){
+    document.write("the background color is black");
+}
+else{
+    document.write("the background color is white");
+}
+let age2 = 22;
+if(age2 < 18){
+    document
+    .write("Junior")
+}
+else if(age2 > 18){
+    document.write("Senior");
+}
+else{
+    document.write("Middle");
+}
+
