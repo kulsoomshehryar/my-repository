6fddc6589c7a0e6cf0b93a6d8e2de5da0877e37a
kulsoomshehryar <kulsoomkulsoomshehryar@gmail.com>
2025-01-06 14:31:23 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+function add(){
+    const num1 = document.getElementById('alphabet1').value
+    const num2 = document.getElementById('alphabet2').value
+    document.getElementById("result").innerHTML = Number(num1) + Number(num2);
+}
+function subtract(){
+    const num1 = document.getElementById('alphabet1').value
+    const num2 = document.getElementById('alphabet2').value
+    document.getElementById("result").innerHTML = Number(num1) - Number(num2);
+}
+function divide(){
+    const num1 = document.getElementById('alphabet1').value
+    const num2 = document.getElementById('alphabet2').value
+    document.getElementById("result").innerHTML = Number(num1) / Number(num2);
+}
+function multiply(){
+    const num1 = document.getElementById('alphabet1').value
+    const num2 = document.getElementById('alphabet2').value
+    document.getElementById("result").innerHTML = Number(num1) * Number(num2);
+}
+function percentage(){
+    const num1 = document.getElementById('alphabet1').value
+    const num2 = document.getElementById('alphabet2').value
+    document.getElementById("result").innerHTML = Number(num1) % Number(num2);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
